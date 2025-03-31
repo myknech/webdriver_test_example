396bd48ee16cf695b9df4f274c8ca9018a8c70af
Alex Ov <alexov@swisscows.email>
2025-03-31 23:19:11 +0300

    Add more tests

--- src/test/java/Test00.java
+++ src/test/java/Test00.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -48,19 +50,58 @@ public class Test00 {
         userName.sendKeys("tomsmith");
 
         WebElement password = driver.findElement(By.id("password"));
-        password.sendKeys("SuperSecretPassword");
+        password.sendKeys("SuperSecretPassword!");
 
         WebElement submitButton = driver.findElement(By.tagName("button"));
         submitButton.click();
 
+
+        // Implicit wait
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
+        // Explicit wait
+
+        // sleep(7000);
+
         WebElement logout = driver.findElement(By.linkText("Logout"));
 
         Assert.assertTrue(logout.isDisplayed());
+
+        // By.linkText("button")
+        // sleep(10000);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Logout")));
+
+        WebElement submitButton2 = driver.findElement(By.linkText("Logout"));
+
+        driver.quit();
+
+    }
+
+
+    @Test
+    public void testImdb00() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.imdb.com/");
+
+        sleep(3000);
+
+        // WebElement menuButton = driver.findElement(By.cssSelector(".ipc-responsive-button__text"));
+        WebElement menuButton = driver.findElement(By.xpath("//*[@class='ipc-responsive-button__text']"));
+
+        menuButton.click();
 
         sleep(5000);
 
         driver.quit();
 
     }
+
 
 }
